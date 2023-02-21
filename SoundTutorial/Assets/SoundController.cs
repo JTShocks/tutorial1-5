5e79b20073abcabf5498cdbfd22a40f7d05519bc
JTShocks <jacobdreyer2002@gmail.com>
2023-02-21 12:50:36 -0500

    Animation commit

--- SoundTutorial/Assets/SoundController.cs
+++ SoundTutorial/Assets/SoundController.cs
@@ -10,6 +10,12 @@ public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
 
+public Animator anim;
+
+void Start()
+{
+  anim = GetComponent<Animator>();
+}
 // Update is called once per frame
 void Update()
 {
@@ -17,25 +23,26 @@ void Update()
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
+          anim.SetInteger("State", 1);
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.L))
